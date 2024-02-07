b0c03bb47538a8614ebba8ba3b4d36a0b0bb9d8f
Wiktoria Skibińska <wikas2000@icloud.com>
2024-02-07 12:11:12 +0100

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './contactform/ContactForm';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      name: '',
+    };
+  }
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexWrap: 'wrap',
+          flexDirection: 'column',
+
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
---
+++ src/components/contactform/ContactForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactForm = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    </div>
+  );
+};
+export default ContactForm;
